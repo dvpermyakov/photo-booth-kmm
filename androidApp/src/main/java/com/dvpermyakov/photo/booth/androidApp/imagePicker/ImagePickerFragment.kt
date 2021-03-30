b748eaa643186d528214435d8a7135f8b99d27b8
dvpermyakov <dvpermyakov1@gmail.com>
2021-03-30 18:31:50 +0400

    add view binding for image picker fragment

--- androidApp/src/main/java/com/dvpermyakov/photo/booth/androidApp/imagePicker/ImagePickerFragment.kt
+++ androidApp/src/main/java/com/dvpermyakov/photo/booth/androidApp/imagePicker/ImagePickerFragment.kt
@@ -2,23 +2,27 @@ package com.dvpermyakov.photo.booth.androidApp.imagePicker
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import androidx.appcompat.widget.AppCompatImageView
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.dvpermyakov.photo.booth.androidApp.InteractorProvider
 import com.dvpermyakov.photo.booth.androidApp.R
+import com.dvpermyakov.photo.booth.androidApp.databinding.FragmentImagePickerBinding
 
 class ImagePickerFragment : Fragment(R.layout.fragment_image_picker) {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val imageView = view.findViewById<AppCompatImageView>(R.id.imageView)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentImagePickerBinding.inflate(inflater)
         val byteArray = (activity as InteractorProvider)
             .getPhotoInteractor()
             .getImages(listOf("imageName"))
             .first()
         val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
-        imageView.setImageBitmap(bitmap)
+        binding.imageView.setImageBitmap(bitmap)
+        return binding.root
     }
 
 }
